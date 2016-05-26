2d322626084d8237661ae59386fc73be3814776c
Luwin <1219810894@qq.com>
2016-05-26 19:22:32 +0800

    init

---
+++ src/main/java/com/jjfly/JjflyApplication.java
@@ -0,0 +1,12 @@
+package com.jjfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjflyApplication.class, args);
+	}
+}
